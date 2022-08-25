3f97fb5d701fe5dd3d0e4d196925371326c838e6
Victor Martins <martinsvictor6781@gmail.com>
2022-08-24 22:49:58 -0300

    deleção de métodos não utilizados

--- code/src/main/java/br/com/leadfortaleza/code/model/animals/AnimalRepository.java
+++ code/src/main/java/br/com/leadfortaleza/code/model/animals/AnimalRepository.java
@@ -16,18 +16,13 @@ public interface AnimalRepository extends PagingAndSortingRepository<Animal, Lon
     @Query("select animal from Animal animal where animal.valid = true")
     Iterable<Animal> findAll();
 
-    //@Query("select animal from Animal animal where animal.valid = true and animal.animalClass = ?1")
-    //Iterable<Animal> findAnimalsByAnimalClass(@Param("animalClass") AnimalClass animalClass);
 
     Iterable<Animal> findAnimalsByNameContainsAndAnimalClassAndValidIsTrue( String name, AnimalClass animalClass);
 
     Iterable<Animal> findAnimalsByNameContainsAndValidIsTrue(String name);
-    //@Query("select animal from Animal animal where animal.weight > :start and animal.weight < :end")
-    //Iterable<Animal> findAnimalsByWeightIsBetween(@Param("start") double start, @Param("end") double end);
 
     Iterable<Animal> findAnimalsByNameContainsAndWeightBetweenAndValidIsTrue(String name, @Min(0) double weight, @Min(0) double weight2);
 
     Iterable<Animal> findAnimalsByNameContainsAndWeightBetweenAndAnimalClassAndValidIsTrue(String name, @Min(0) double weight, @Min(0) double weight2, AnimalClass animalClass);
 
-    //Iterable<Animal> findAnimalsByWeightIsBetweenAndAnimalClassAndValid(@Min(0) double weight, @Min(0) double weight2, AnimalClass animalClass, boolean valid);
 }
